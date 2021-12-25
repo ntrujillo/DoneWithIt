7b07706ef173166338f79fedee9afab595973a46
Wladimir Trujillo <wladimir.trujillo.ec@gmail.com>
2021-12-25 13:53:06 -0500

    finishi section input components

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   StyleSheet,
   Platform,
   Dimensions,
-  View
+  View,
+  TextInput,
+  Text
 } from 'react-native';
 
 import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks'
@@ -13,15 +15,31 @@ import Screen from './app/components/Screen';
 import Icon from './app/components/Icon';
 import AccountScreen from './app/screens/AccountScreen';
 import ListingsScreen from './app/screens/ListingsScreen';
+import AppTextInput from './app/components/AppTextInput';
+import AppPicker from './app/components/AppPicker';
+
+const categories = [
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Cameras", value: 3 },
+]
 
 export default function App() {
+  const [category, setCategory] = useState(categories[0]);
+  const [firstName, setFirstName] = useState('');
   const handlePress = () => console.log(require('./app/assets/icon.png'));
   console.log(Dimensions.get('screen'))
   console.log(useDimensions());
   const orientation = useDeviceOrientation();
   console.log(orientation);
   return (
-    <ListingsScreen></ListingsScreen>
+    <Screen>
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={item => setCategory(item)}
+        items={categories} icon="apps" placeholder="Category" />
+      <AppTextInput icon="email" placeholder="Email" />
+    </Screen>
   );
 }
 
--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,28 +1,12 @@
 import React from 'react';
-import { Platform, StyleSheet, Text } from 'react-native';
+import { Text } from 'react-native';
+import defaultStyles from '../config/styles';
 
 function AppText({ children, style }) {
     return (
-        <Text style={[styles.text], style}>{children}</Text>
+        <Text style={[defaultStyles.text], style}>{children}</Text>
     );
 }
 
-
-
-const styles = StyleSheet.create({
-    text: {
-        color: 'tomato',
-        ...Platform.select({
-            ios: {
-                fontSize: 20,
-                fontFamily: 'Avenir'
-            },
-            android: {
-                fontSize: 18,
-                fontFamily: 'Roboto'
-            }
-        })
-    }
-});
 
 export default AppText;
