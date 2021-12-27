e880008eac32da0b07958ddc47dce9ccc4e9c889
Wladimir Trujillo <wladimir.trujillo.ec@gmail.com>
2021-12-26 22:32:16 -0500

    fix categories

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -20,15 +20,60 @@ const validationSchema = Yup.object().shape({
 });
 
 const categories = [
-    { label: 'Furniture', value: 1, backgroundColor: 'red', icon: 'apps' },
-    { label: 'Cars', value: 2, backgroundColor: 'orange', icon: 'car' },
-    { label: 'Cameras', value: 3, backgroundColor: 'yellow', icon: 'camera' },
-    { label: 'Games', value: 4, backgroundColor: 'green', icon: 'lock' },
-    { label: 'Clothing', value: 5, backgroundColor: 'green', icon: 'lock' },
-    { label: 'Sports', value: 6, backgroundColor: 'blue', icon: 'lock' },
-    { label: 'Movies & Music', value: 7, backgroundColor: 'blue', icon: 'lock' },
-    { label: 'Books', value: 8, backgroundColor: 'purple', icon: 'book' },
-    { label: 'Other', value: 9, backgroundColor: 'gray', icon: 'lock' },
+    {
+        backgroundColor: "#fc5c65",
+        icon: "floor-lamp",
+        label: "Furniture",
+        value: 1,
+    },
+    {
+        backgroundColor: "#fd9644",
+        icon: "car",
+        label: "Cars",
+        value: 2,
+    },
+    {
+        backgroundColor: "#fed330",
+        icon: "camera",
+        label: "Cameras",
+        value: 3,
+    },
+    {
+        backgroundColor: "#26de81",
+        icon: "cards",
+        label: "Games",
+        value: 4,
+    },
+    {
+        backgroundColor: "#2bcbba",
+        icon: "shoe-heel",
+        label: "Clothing",
+        value: 5,
+    },
+    {
+        backgroundColor: "#45aaf2",
+        icon: "basketball",
+        label: "Sports",
+        value: 6,
+    },
+    {
+        backgroundColor: "#4b7bec",
+        icon: "headphones",
+        label: "Movies & Music",
+        value: 7,
+    },
+    {
+        backgroundColor: "#a55eea",
+        icon: "book-open-variant",
+        label: "Books",
+        value: 8,
+    },
+    {
+        backgroundColor: "#778ca3",
+        icon: "application",
+        label: "Other",
+        value: 9,
+    },
 ];
 
 function ListingEditScreen(props) {
